654d2a2274f9d377cc9dacc86dd3a9e1d01acc13
taiki0304 <taiki@192.168.11.7>
2018-08-04 22:15:02 +0900

    [add] first commit

---
+++ eternal-app/src/main/java/com/eternal/web/EternalAppApplication.java
@@ -0,0 +1,12 @@
+package com.eternal.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalAppApplication.class, args);
+	}
+}
