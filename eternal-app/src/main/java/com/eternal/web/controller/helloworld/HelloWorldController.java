878902f99563f1838d730325824edb3e8f396084
taiki0304-vaio <f1x.mi1kt2748@gmail.com>
2018-09-17 21:17:02 +0900

    [add] API用のテストメソッドを作成
    ・HelloWorldController#helloJson()
    
    [fix] デフォルトのサーバーポート番号を変更
    
    ・8080->8090

--- eternal-app/src/main/java/com/eternal/web/controller/helloworld/HelloWorldController.java
+++ eternal-app/src/main/java/com/eternal/web/controller/helloworld/HelloWorldController.java
@@ -1,13 +1,36 @@
 package com.eternal.web.controller.helloworld;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
+	/**
+	 * 疎通確認用のテストに使用する
+	 * @return
+	 */
 	@RequestMapping("/hello")
 	public String helloWorld() {
 		return "/hello";
+	}
+
+	/**
+	 * webサーバーとのAPI通信のテストに使用する
+	 * @return
+	 */
+	@RequestMapping(value="/hellojson", method=RequestMethod.GET)
+	@ResponseBody
+	public List<String> helloJson() {
+		List<String> nameList = new ArrayList<>();
+		nameList.add("Taro");
+		nameList.add("Jiro");
+		nameList.add("Jack");
+		return nameList;
 	}
 }
